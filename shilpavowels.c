7ae4397c72ebbfc61fcf46b08cd97a1a6b9dcd92
Shilparajan <31357790+Shilparajan@users.noreply.github.com>
2017-09-05 11:34:39 +0530

    Add files via upload

---
+++ shilpavowels.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+main()
+{
+char para[100];
+int vowels, i;
+printf("Enter the sentence:%s\n", para);
+scanf("%c",&para);
+vowels=0;
+while(i<100)
+{
+scanf("%s",&para[i]);
+if(para[i]=a);
+vowels++;
+
+  if(para[i]=e);
+vowels++;
+
+  if(para[i]=i);
+vowels++;
+
+  if(para[i]=o);
+vowels++;
+
+if(para[i]=u);
+vowels++;
+
+i++;
+}
+scanf("%d",&vowels);
+printf("The total number of vowels present in the given sentence is:%d",vowels);
+}
+
+
