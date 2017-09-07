9099c628a69d2d13519863ed0504bb71b1f97a89
Shilparajan <31357790+Shilparajan@users.noreply.github.com>
2017-09-07 10:36:21 +0530

    Add files via upload
    module to generate random numbers

---
+++ 6.aug.2017/shilparandom.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+int currenttempr;
+srand(3045);
+currenttempr= rand();
+printf("%d\n",currenttempr);
+currenttempr= rand();
+printf("%d\n",currenttempr);
+
+srand(4300);
+currenttempr= rand();
+    printf("%d\n",currenttempr);
+  currenttempr= rand();
+    printf("%d\n",currenttempr);
+  currenttempr= rand();
+    printf("%d\n",currenttempr);
+
+     srand(2490);
+currenttempr= rand();
+    printf("%d\n",currenttempr);
+  currenttempr= rand();
+    printf("%d\n",currenttempr);
+  currenttempr= rand();
+    printf("%d\n",currenttempr);
+}
